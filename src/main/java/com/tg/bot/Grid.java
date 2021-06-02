4a4190a947c5c09b0f2ed55c586e670896dae96d
thien <thienkaito@gmail.com>
2021-06-02 20:10:07 +0300

    Add Grid.java

---
+++ src/main/java/com/tg/bot/Grid.java
@@ -0,0 +1,11 @@
+package com.tg.bot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grid {
+    int numAttempts;
+    public static final int nRows = 10, nCols = 10;
+    char[][] cells = new char[nRows][nCols];
+    List<String> Solutions = new ArrayList<>();
+}
